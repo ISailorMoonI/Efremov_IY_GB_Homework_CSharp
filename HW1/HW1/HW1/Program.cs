cd82bac67b15e54f3163bcf7adf20402c6fa8d5f
SailorMoon <yashamaru99@gmail.com>
2021-01-17 11:19:53 +0300

    Complete HW for Lesson1

---
+++ HW1/HW1/HW1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}, текущее время {DateTime.Now} ");
+        }
+    }
+}
