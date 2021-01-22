154325988c57cb7b346ce76be29b1d438ca2390e
SailorMoon <yashamaru99@gmail.com>
2021-01-23 02:18:41 +0300

    Completed HW2

---
+++ HW1/Homework1/HW2 Task 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HW2_Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string company = "ИП Зеленый Cлоник";
+            var date = DateTime.Now;
+            string cashier = "Братишкова И.В"; 
+            double hlebushek = 69.99;
+            double vilka = 14.78;
+            double sum = hlebushek + vilka;
+            double payment = 1000;
+            double tax = (sum * 10 / 100);
+            double change = payment - sum - tax;
+            
+            Console.WriteLine($"{company}");
+            Console.WriteLine($"------------------------------");
+            Console.WriteLine($"{date}");
+            Console.WriteLine($"{cashier}");
+            Console.WriteLine($"------------------------------");
+            Console.WriteLine($"Хлебушек сладенький 1x = {hlebushek}");
+            Console.WriteLine($"Вилка сантехническая 1х = {vilka}");
+            Console.WriteLine($"------------------------------");
+            Console.WriteLine($"Итого = {sum}");
+            Console.WriteLine($"Внесено наличными = {payment}");
+            Console.WriteLine($"НДС {tax}");
+            Console.WriteLine($"Cдача {change}");
+            
+
+
+        }
+    }
+}
