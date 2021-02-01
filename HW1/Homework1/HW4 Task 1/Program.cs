ba1adce10832a9b49ca9f8b90d5394f5e13ef56a
SailorMoon <yashamaru99@gmail.com>
2021-02-01 22:47:11 +0300

    Completed hw4

---
+++ HW1/Homework1/HW4 Task 1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace HW4_Task_1
+{
+    class Program
+    {
+        static (string firstname, string lastname, string petronymic) GetFullName()
+        {
+            Console.WriteLine("Укажите имя");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Укажите фамилию");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Укажите отчество");
+            string petronymic = Console.ReadLine();
+            return (firstname, lastname, petronymic);
+        }
+        static (string name, string lastname, string petronymic)[] CreateUsers()
+        {
+            Console.WriteLine("Введите количество пользователей:");
+            int count = ReadInt();
+            (string firstname, string lastname, string petronymic)[] users = new (string firstname, string lastname, string petronymic)[count];
+            for (int i = 0; i < users.Length; i++)
+            {
+                users[i] = GetFullName();
+                Console.WriteLine($"Пользователь {FormatUserData(users[i])} сохранен");
+            }
+
+            Console.WriteLine("Ввод данных завершён. Нажмите любую клавишу...");
+            Console.ReadKey();
+            Console.Clear();
+
+            return users;
+        }
+        static int ReadInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        static void PrintUser((string firstname, string lastname, string petronymic) user)
+        {
+            Console.WriteLine(FormatUserData(user));
+        }
+
+        static void PrintUsers((string firstname, string lastname, string petronymic)[] users)
+        {
+            Console.WriteLine("Вывод базы данных:");
+            for (int i = 0; i < users.Length; i++)
+            {
+                PrintUser(users[i]);
+            }
+        }
+
+        static string FormatUserData((string name, string lastname, string petronymic) user)
+        {
+            return $"({user.name}) ({user.lastname}) ({user.petronymic})";
+        }
+
+
+        static void Main(string[] args)
+        {
+            (string firstname, string lastname, string petronymic)[] users = CreateUsers();
+            PrintUsers(users);
+
+        }
+    }
+}
---
+++ HW1/Homework1/HW4 Task 3/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace HW4_Task_3
+{
+    class Program
+    {
+        [Flags]
+        enum Season
+        {
+            Spring,
+            Summer,
+            Autumn,
+            Winter
+        }
+
+        static string timeYear(int numberMounth)
+        {
+            string whatIsNow;
+            switch (numberMounth)
+            {
+                case 1:
+                    Season currentSeason = Season.Winter;
+                    whatIsNow = Convert.ToString(currentSeason);
+                    return whatIsNow;
+                    break;
+                case 2:
+                    currentSeason = Season.Winter;
+                    whatIsNow = Convert.ToString(currentSeason);
+                    return whatIsNow;
+                    break;
+                case 3:
+                    currentSeason = Season.Spring;
+                    whatIsNow = Convert.ToString(currentSeason);
+                    return whatIsNow;
+                    break;
+                case 4:
+                    currentSeason = Season.Spring;
+                    whatIsNow = Convert.ToString(currentSeason);
+                    return whatIsNow;
+                    break;
+                case 5:
+                    currentSeason = Season.Spring;
+                    whatIsNow = Convert.ToString(currentSeason);
+                    return whatIsNow;
+                    break;
+                case 6:
+                    currentSeason = Season.Summer;
+                    whatIsNow = Convert.ToString(currentSeason);
+                    return whatIsNow;
+                    break;
+                case 7:
+                    currentSeason = Season.Summer;
+                    whatIsNow = Convert.ToString(currentSeason);
+                    return whatIsNow;
+                    break;
+                case 8:
+                    currentSeason = Season.Summer;
+                    whatIsNow = Convert.ToString(currentSeason);
+                    return whatIsNow;
+                    break;
+                case 9:
+                    currentSeason = Season.Autumn;
+                    whatIsNow = Convert.ToString(currentSeason);
+                    return whatIsNow;
+                    break;
+                case 10:
+                    currentSeason = Season.Autumn;
+                    whatIsNow = Convert.ToString(currentSeason);
+                    return whatIsNow;
+                    break;
+                case 11:
+                    currentSeason = Season.Autumn;
+                    whatIsNow = Convert.ToString(currentSeason);
+                    return whatIsNow;
+                    break;
+                case 12:
+                    currentSeason = Season.Winter;
+                    whatIsNow = Convert.ToString(currentSeason);
+                    return whatIsNow;
+                    break;
+                default:
+                    whatIsNow = "Ошибка: введите число от 1 до 12";
+                    return whatIsNow;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число месяца: ");
+
+            int numberMounth = int.Parse(Console.ReadLine());
+
+            string result = timeYear(numberMounth);
+
+            Console.WriteLine($"\nТекущий сезон: {result}");
+
+        }
+    }
+}
