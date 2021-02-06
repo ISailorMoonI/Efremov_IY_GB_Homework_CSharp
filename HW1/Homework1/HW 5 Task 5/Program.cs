1114e883b8554270389491a29a81f8bd5592f474
SailorMoon <yashamaru99@gmail.com>
2021-02-06 13:09:11 +0300

    HW5 task 1,2,3 complete.

---
+++ HW1/Homework1/HW 5 Task 5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_5_Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
